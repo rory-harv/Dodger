c8be7d309c724738ca3eb34ff4258757658592e5
Lucas Cordova <lpcordova@outlook.com>
2025-10-20 08:59:21 -0700

    Init

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{   
+    [SerializeField] float moveSpeed = 10f;
+    
+    void Start() 
+    {        
+        PrintInstruction();
+    }
+
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void PrintInstruction()
+    {
+        Debug.Log("Welcome to the game!");
+        Debug.Log("Move using arrow keys or wasd");
+        Debug.Log("Don't bump into objects!");   
+    }
+
+    void MovePlayer()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float yValue = 0f;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        transform.Translate(xValue, yValue, zValue);
+    }
+
+}
