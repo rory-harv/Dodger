cde97e36121a7c155f55b15dd1eb80deb1c60960
rory <victoria.harvey1@icloud.com>
2025-11-06 10:33:37 -0800

    updated livesleft

--- Assets/Scripts/Scorer.cs
+++ Assets/Scripts/Scorer.cs
@@ -8,15 +8,19 @@ public class Scorer : MonoBehaviour
     int hits = 0;
     private float elapsedTime = 0f;
     private float score = 0f;
+    private float livesleft = 3f;
     public float scoreMultiplier = 10f;
     public UIDocument uiDocument;
     private Label scoreText;
+    private Label livesText;
     private Button restartButton;
     public bool finished = false;
+    public bool collision = false;
 
     void Start()
     {
         scoreText = uiDocument.rootVisualElement.Q<Label>("ScoreLabel");
+        livesText = uiDocument.rootVisualElement.Q<Label>("LivesLeft");
         restartButton = uiDocument.rootVisualElement.Q<Button>("RestartButton");
         restartButton.clicked += ReloadScene;
         restartButton.style.display = DisplayStyle.None;
@@ -29,11 +33,18 @@ public class Scorer : MonoBehaviour
             elapsedTime += Time.deltaTime;
             score = Mathf.FloorToInt(elapsedTime * scoreMultiplier);
             scoreText.text = "Score: " + score;
+            livesText.text = "Lives Left: " + livesleft;
+            if (collision == true && livesleft > -1)
+            {
+                livesleft--;
+            }
         }
     }
 
     private void OnCollisionEnter(Collision other)
     {
+        collision = true;
+        Update();
         if (other.gameObject.tag != "Hit")
         {
             hits++;
@@ -41,10 +52,13 @@ public class Scorer : MonoBehaviour
             if (hits >= 3)
             {
                 Debug.Log("Game Over");
+                livesleft = 0;
+                livesText.text = "Lives Left: " + livesleft;
                 restartButton.style.display = DisplayStyle.Flex;
                 finished = true;
             }
         }
+        collision = false;
     }
     void ReloadScene()
     {
